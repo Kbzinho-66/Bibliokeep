52ae5ba6d38002b7632d2eda876a609556514f1c
Kbzinho-66 <gvalles@ucs.br>
2022-04-17 16:47:16 -0300

    Criação de um Enum

---
+++ Codigos.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Cod(Enum):
+    CADASTRO  = 1
+    ALTERAR   = 2
+    DELETAR   = 3
+    CONSULTAR = 4
+    SAIR      = 0
+
+    TITULO  = 1
+    AUTOR   = 2
+    ANO_EDI = 3
