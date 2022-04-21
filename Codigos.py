be19d95445636023411606e112cbf20d8ef6125d
Kbzinho-66 <gvalles@ucs.br>
2022-04-21 17:53:29 -0300

    Separação dos códigos em classes mais adequadas

--- Codigos.py
+++ Codigos.py
@@ -1,12 +1,13 @@
 from enum import IntEnum
 
-class Cod(IntEnum):
+class Opcao(IntEnum):
     CADASTRO  = 1
     ALTERAR   = 2
     DELETAR   = 3
     CONSULTAR = 4
     SAIR      = 0
 
+class Filtro(IntEnum):
     TITULO  = 1
     AUTOR   = 2
     ANO_EDI = 3
