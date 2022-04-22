292f95e0c1682bd064357d6fefd060331bdc65f0
Kbzinho-66 <gvalles@ucs.br>
2022-04-22 00:37:50 -0300

    Primeiro teste com inserção

--- Servidor.py
+++ Servidor.py
@@ -6,15 +6,6 @@ from Codigos import Cod
 
 def main():
 
-    banco_livros = psycopg2.connect(
-        host     = 'localhost',
-        database = 'livros',
-        user     = 'postgres',
-        password = 'postgres'
-    )
-
-    db = banco_livros.cursor()
-
     ip    = 'localhost'
     porta = 12000
 
@@ -34,18 +25,31 @@ def main():
             break
     
     s.close()
-    db.close()
-
-    if banco_livros is not None:
-        banco_livros.close()
 
 
 def trata_mensagem(opcao: Cod, livros: List[Livro]) -> str:
+
+    banco_livros = psycopg2.connect(
+        host     = 'localhost',
+        database = 'livros',
+        user     = 'postgres',
+        password = 'postgres'
+    )
+
+    db = banco_livros.cursor()
+
     if opcao == Cod.CADASTRO:
         livro = livros[0]
-        # TODO Cadastrar o livro
-        # TODO Retornar se deu pra cadastrar ou não
-        return 'Livro inserido com sucesso.'
+        db.execute('SELECT max(codigo) from livrostemp')        
+        codigo = db.fetchone()
+        # TODO Tem que inserir o nome do livro na tabela livros, o autor na autor, ...
+        db.execute(
+            ''' INSERT INTO livrostemp (codigo, titulo, autor, edicao, ano)
+            VALUES (%s, %s, %s, %s, %s); ''', 
+            (codigo, livro.nome, livro.autor, livro.edicao, livro.ano_pub)
+        )
+        banco_livros.commit()
+        msg = 'Livro inserido com sucesso.'
     elif opcao == Cod.CONSULTAR:
         # TODO Consultar os livros que se encaixam no filtro
         # TODO Retornar a lista de livros
@@ -59,9 +63,17 @@ def trata_mensagem(opcao: Cod, livros: List[Livro]) -> str:
         # TODO Retornar a lista de livros
         pass 
     elif opcao == Cod.SAIR:
-        return 'Servidor fechado automaticamente.'
+        msg = 'Servidor fechado automaticamente.'
     else:
-        return 'Opção inválida.'
+        msg = 'Opção inválida.'
+
+    db.close()
+
+    if banco_livros is not None:
+        banco_livros.close()
+
+    return msg
+
 
 if __name__ == '__main__':
     main()
