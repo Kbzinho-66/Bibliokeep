4a519b639ad0e3f2f747761120b78fea8a96ce86
Kbzinho-66 <gvalles@ucs.br>
2022-04-23 21:42:03 -0300

    Tive que adicionar um Filtro pra não quebrar uma tipagem

--- Codigos.py
+++ Codigos.py
@@ -1,13 +1,16 @@
 from enum import IntEnum
 
+
 class Opcao(IntEnum):
-    CADASTRO  = 1
-    ALTERAR   = 2
-    DELETAR   = 3
+    CADASTRO = 1
+    ALTERAR = 2
+    DELETAR = 3
     CONSULTAR = 4
-    SAIR      = 0
+    SAIR = 0
+
 
 class Filtro(IntEnum):
-    TITULO  = 1
-    AUTOR   = 2
+    TITULO = 1
+    AUTOR = 2
     ANO_EDI = 3
+    SAIR = 0
