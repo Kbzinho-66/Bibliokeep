5ab5ae604cfd027d769b0a9730e0221231ee3660
Kbzinho-66 <gvalles@ucs.br>
2022-05-25 23:02:18 -0300

    Nevermind....

---
+++ Sockets/Codigos.py
@@ -0,0 +1,17 @@
+from enum import IntEnum
+
+
+class Opcao(IntEnum):
+    CADASTRO = 1
+    ALTERAR = 2
+    DELETAR = 3
+    CONSULTAR = 4
+    FILTRAR = 5
+    SAIR = 0
+
+
+class Filtro(IntEnum):
+    TITULO = 1
+    AUTOR = 2
+    ANO_EDI = 3
+    SAIR = 0
